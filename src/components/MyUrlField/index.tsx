ad587b03b1e1a419aee5626c55ba40bd064df277
Vinícius Neto <viniciusbsneto@gmail.com>
2020-11-07 16:40:00 -0300

    chore: Change MyUrlField component style

--- src/components/MyUrlField/index.tsx
+++ src/components/MyUrlField/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { UrlFieldProps } from 'react-admin';
+import { makeStyles } from '@material-ui/core/styles';
+import LaunchIcon from '@material-ui/icons/Launch';
+
+const useStyles = makeStyles({
+  link: {
+      textDecoration: 'none',
+  },
+  icon: {
+      width: '0.5em',
+      paddingLeft: 2,
+  },
+});
 
 const MyUrlField: React.FC<UrlFieldProps> = ({ record, source }) => {
+  const classes = useStyles();
   
   if (record && source) {
-    return <a href={record[source]}>{record[source]}</a>;
+    return <a className={classes.link} href={record[source]}>{record[source]}<LaunchIcon className={classes.icon} /></a>;
   } else {
     return <a href="/#">No URL defined</a>;
   }
