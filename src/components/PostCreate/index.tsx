53d32d3bcdf2cca27f045087572f3f0de083aee6
Vinícius Neto <viniciusbsneto@gmail.com>
2020-11-07 17:37:23 -0300

    feat: Add PostCreate component

---
+++ src/components/PostCreate/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Create, SimpleForm, ReferenceInput, SelectInput, TextInput, CreateProps } from 'react-admin';
+
+const PostCreate: React.FC<CreateProps> = (props) => {
+  return (
+    <Create {...props}>
+      <SimpleForm>
+        <ReferenceInput source="userId" reference="users">
+          <SelectInput optionText="name" />
+        </ReferenceInput>
+        <TextInput source="title" />
+        <TextInput source="body" multiline />
+      </SimpleForm>
+    </Create>
+  );
+}
+
+export default PostCreate;
