dc255e61a4d59fed58710f84b6bad15037c2c5a9
Vinícius Neto <viniciusbsneto@gmail.com>
2020-12-11 22:35:06 -0300

    feat: Add PostFilter to PostList component

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { List, Datagrid, TextField, ReferenceField, ListProps, EditButton } from 'react-admin';
+import PostFilter from '../PostFilter';
 
-const PostList: React.FC<ListProps> = (props) => {
-  return (
-    <List {...props}>
-      <Datagrid>
-        <TextField source="id" />
-          <ReferenceField source="userId" reference="users">
-              <TextField source="name" />
-          </ReferenceField>
-          <TextField source="title" />
-          <EditButton />
-      </Datagrid>
-    </List>
-  );
-}
+const PostList: React.FC<ListProps> = (props) => (
+  <List filters={<PostFilter />} {...props}>
+    <Datagrid>
+      <TextField source="id" />
+        <ReferenceField source="userId" reference="users">
+          <TextField source="name" />
+        </ReferenceField>
+        <TextField source="title" />
+        <EditButton />
+    </Datagrid>
+  </List>
+);
 
 export default PostList;
