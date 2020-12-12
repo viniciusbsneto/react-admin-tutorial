7ce024c3bc9b201c724ee409e082f1de66a6bff5
Vinícius Neto <viniciusbsneto@gmail.com>
2020-12-11 22:49:35 -0300

    feat: Add UserIcon to users resource menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import UserIcon from '@material-ui/icons/Group';
+
 import UserList from './components/Users/UserList';
 import PostList from './components/Posts/PostList';
 import PostEdit from './components/Posts/PostEdit';
@@ -10,7 +12,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App: React.FC = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
   </Admin>
 );
