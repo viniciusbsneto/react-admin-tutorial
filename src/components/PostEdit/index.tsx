50f0000b39b72f75ec83f87dcd30b3866611e8e9
Vinícius Neto <viniciusbsneto@gmail.com>
2020-12-11 22:19:35 -0300

    feat: Use PostTitle component as PostEdit title

--- src/components/PostEdit/index.tsx
+++ src/components/PostEdit/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Edit, SimpleForm, ReferenceInput, SelectInput, TextInput, EditProps } from 'react-admin';
 
+import PostTitle from '../PostTitle';
+
 const PostEdit: React.FC<EditProps> = (props) => {
   return (
-    <Edit {...props}>
+    <Edit title={<PostTitle />} {...props}>
       <SimpleForm>
         <TextInput source="id" />
         <ReferenceInput source="userId" reference="users">
