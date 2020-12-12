7146f4056bfde3ec85270b8f9055f7e118a6e684
Vinícius Neto <viniciusbsneto@gmail.com>
2020-12-11 23:42:07 -0300

    feat: Add PostList as SimpleList on small screens

--- src/components/Posts/PostList/index.tsx
+++ src/components/Posts/PostList/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField, ListProps, EditButton } from 'react-admin';
+import { List, Datagrid, TextField, ReferenceField, ListProps, EditButton, SimpleList } from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
 import PostFilter from '../PostFilter';
 
-const PostList: React.FC<ListProps> = (props) => (
-  <List filters={<PostFilter />} {...props}>
-    <Datagrid>
-      <TextField source="id" />
-        <ReferenceField source="userId" reference="users">
-          <TextField source="name" />
-        </ReferenceField>
-        <TextField source="title" />
-        <EditButton />
-    </Datagrid>
-  </List>
-);
+const PostList: React.FC<ListProps> = (props) => {
+  const isSmall = useMediaQuery<any>((theme) => theme.breakpoints.down('sm'));
+  
+  return (
+    <List filters={<PostFilter />} {...props}>
+      {isSmall ? (
+        <SimpleList
+          primaryText={record => record.user}
+          secondaryText={record => record.title}
+        />
+      ) : (
+        <Datagrid>
+          <TextField source="id" />
+          <ReferenceField source="userId" reference="users">
+            <TextField source="name" />
+          </ReferenceField>
+          <TextField source="title" />
+          <EditButton />
+        </Datagrid>
+      )}
+    </List>
+  )
+};
 
 export default PostList;
