de821aaff27991871c45e07c3fe940b9cfbed143
Vinícius Neto <viniciusbsneto@gmail.com>
2020-12-11 22:50:00 -0300

    feat: Add PostIcon to posts resource menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import UserIcon from '@material-ui/icons/Group';
+import PostIcon from '@material-ui/icons/Book';
 
 import UserList from './components/Users/UserList';
 import PostList from './components/Posts/PostList';
@@ -13,7 +14,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App: React.FC = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="users" list={UserList} icon={UserIcon} />
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
   </Admin>
 );
 
